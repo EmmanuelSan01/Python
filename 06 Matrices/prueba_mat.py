b997be96e5d55a0045a8557f50131a10222c93fd
EmmanuelSan01 <114524605+EmmanuelSan01@users.noreply.github.com>
2024-09-19 21:19:40 -0500

    Add files via upload

---
+++ 06 Matrices/prueba_mat.py
@@ -0,0 +1,12 @@
+from funciones_mat import *
+
+matriz=crearMat(2,3)
+printMat(matriz)
+
+#inputMat(matriz)
+randomMat(matriz,10,100)
+printMat(matriz)
+
+matrizI=crearMat(4,4)
+matrizI=matrizIdentidad(matrizI)
+printMat(matrizI)
